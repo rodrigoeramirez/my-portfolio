3ba781d9e40aa76526b991ea100146a758ee5784
rodriramirez6 <rodrigoramirezok@gmail.com>
2025-03-27 22:25:11 -0300

    fix's

--- app/(routes)/tech-stack/page.tsx
+++ app/(routes)/tech-stack/page.tsx
@@ -4,37 +4,47 @@ import TransitionPage from "@/components/transition-page";
 import { databases, languages, tools } from "@/data";
 import { Fragment } from "react";
 
+// Definir un tipo para los items en `data`
+interface SkillItem {
+  id: number;
+  title: string;
+  image: string;
+}
 
-const SkillSection = ({ title, data }) => {
-    return (
-      <div className="relative z-10 p-4 mt-6 max-w-6xl mx-auto">
-        <h3 className="mb-4 text-xl">
-          {title}
-        </h3>
-        <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-5 xl:grid-cols-9 gap-4"> {/* Reducimos el gap */}
-          {data.map((item) => (
-            <ContainerSkills key={item.id} data={item} />
-          ))}
-        </div>
+
+interface SkillSectionProps {
+  title: string;
+  data: SkillItem[];
+}
+
+const SkillSection = ({ title, data }: SkillSectionProps) => {
+  return (
+    <div className="relative z-10 p-4 mt-6 max-w-6xl mx-auto">
+      <h3 className="mb-4 text-xl">
+        {title}
+      </h3>
+      <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-5 xl:grid-cols-9 gap-4">
+        {data.map((item) => (
+          <ContainerSkills key={item.id} data={item} />
+        ))}
       </div>
-    );
-  };
-  
-  
-  
-  export default function TechStack() {
-    return (
-      <Fragment>
-        <TransitionPage />
-        <ContainerPage>
-        <h1 className="text-2xl leading-tight text-center md:text-left md:text-5xl md:mt-10">Tech {' '}
-                    <span className="font-bold text-secondary">Stack</span>
-                </h1>
-          <SkillSection title="Languages & Frameworks" data={languages} />
-          <SkillSection title="Tools" data={tools} />
-          <SkillSection title="Databases" data={databases} />
-        </ContainerPage>
-      </Fragment>
-    );
-  }
-  
+    </div>
+  );
+};
+
+export default function TechStack() {
+  return (
+    <Fragment>
+      <TransitionPage />
+      <ContainerPage>
+        <h1 className="text-2xl leading-tight text-center md:text-left md:text-5xl md:mt-10">
+          Tech {' '}
+          <span className="font-bold text-secondary">Stack</span>
+        </h1>
+        <SkillSection title="Languages & Frameworks" data={languages} />
+        <SkillSection title="Tools" data={tools} />
+        <SkillSection title="Databases" data={databases} />
+      </ContainerPage>
+    </Fragment>
+  );
+}
