06b4591f9c1b68b4cb6a7d6d9835a70cd324c61a
rodriramirez6 <rodrigoramirezok@gmail.com>
2025-03-27 22:38:37 -0300

    margin botom introduction

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main>
       <TransitionPage/>
-      <div className="flex min-h-[100vh] h-full bg-no-repeat bg-gradient-cover">
+      <div className="flex min-h-[100vh] h-full bg-no-repeat bg-gradient-cover ">
          <CoverParticles/>
          <Introduction></Introduction>
       </div>   
--- components/introduction.tsx
+++ components/introduction.tsx
@@ -38,7 +38,7 @@ const Introduction = () => {
           <p className="mx-auto mb-3 text-xl md:mx-0 md:mb-8 text-center md:text-left">
             a Systems Information Analyst and Developer based in Argentina. I am passionate about web development and have experience in both backend and frontend technologies. You can learn more about me on my LinkedIn page.
           </p>
-          <div className="flex item-center justify-center gap-3 md:justify-start md:gap-10">
+          <div className="flex item-center justify-center gap-3 md:justify-start md:gap-10 mb-30">
             <Link href="/my-projects" className="px-3 py-2 transition-all border-2 cursor-pointer text-md w-fir rounded-xl hover:shadow-xl hover:shadow-white/50">
               View projects
             </Link>
