0858503a2dead57ee9a6f13b98ae913d20615d20
rodriramirez6 <rodrigoramirezok@gmail.com>
2025-07-17 20:15:35 -0300

    Se agregaron algunos lenguajes nuevos y update de cv

--- data.tsx
+++ data.tsx
@@ -1,4 +1,4 @@
-import { HomeIcon, UserRound, Linkedin, Crop, Pencil, Computer, Book, Rocket, Speech, Code, FolderOpen, Github, Youtube } from "lucide-react";
+import { HomeIcon, UserRound, Linkedin, Speech, Code, FolderOpen, Github } from "lucide-react";
 
 export const socialNetworks = [
 
@@ -51,13 +51,6 @@ export const itemsNavbar = [
 export const dataAboutPage = [
     {
         id: 1,
-        title: "Developer",
-        subtitle: "Registro de la Propiedad de La Provincia de Buenos Aires",
-        description: "In my role as a developer, I program in natural language to maintain systems that handle large volumes of data. Additionally, I implement new features to enhance the functionality of the organization.",
-        date: "2025 ",
-    },
-    {
-        id: 2,
         title: "Specialist in Equipment and Network Maintenance",
         subtitle: "Registro de la Propiedad de La Provincia de Buenos Aires",
         description: "In addition to the routine tasks related to equipment and network maintenance, I was responsible for developing custom applications and scripts to optimize and streamline processes, improving efficiency and reducing work time.",
@@ -65,35 +58,6 @@ export const dataAboutPage = [
     },
 
 ]
-
-
-export const serviceData = [
-    {
-        icon: <Crop />,
-        title: "Branding",
-        description: "Desarrollo de una identidad de marca sólida y coherente, incluyendo diseño de logotipo, colores y elementos visuales",
-    },
-    {
-        icon: <Pencil />,
-        title: "Diseño web",
-        description: "Diseño creativo y profesional de interfaces web intuitivas y atractivas, centradas en la experiencia del usuario",
-    },
-    {
-        icon: <Computer />,
-        title: "Desarrollo web",
-        description: "Diseño y desarrollo de sitios web a medida, adaptados a tus necesidades",
-    },
-    {
-        icon: <Book />,
-        title: "Copywriting",
-        description: "Creación de contenido persuasivo y atractivo que capta la atención de tu audiencia",
-    },
-    {
-        icon: <Rocket />,
-        title: "SEO",
-        description: "Optimización de tu presencia en línea mediante estrategias de SEO avanzadas, ",
-    },
-];
 
 export const dataPortfolio = [
     {
@@ -187,7 +151,18 @@ export const databases=[
         id: 2,
         title: "SQL Server",
         image: "/sql_server.png",
+    },
+    {
+        id: 3,
+        title: "PostgreSQL",
+        image: "/postgre_sql.png",
+    },
+    {
+        id: 4,
+        title: "Adabas",
+        image: "/adabas.png",
     }
+
 ]
 
 export const languages = [
@@ -235,51 +210,11 @@ export const languages = [
         id: 8,
         title: "Tailwind CSS",
         image: "/tailwind.png",
+    },
+    {
+        id: 9,
+        title: "Natural",
+        image: "/natural.png",
     }
     
-];
-
-export const dataTestimonials = [
-    {
-        id: 1,
-        name: "George Snow",
-        description:
-            "¡Increíble plataforma! Los testimonios aquí son genuinos y me han ayudado a tomar decisiones informadas. ¡Altamente recomendado!",
-        imageUrl: "/profile1.png",
-    },
-    {
-        id: 2,
-        name: "Juan Pérez",
-        description:
-            "Me encanta la variedad de testimonios disponibles en esta página. Es inspirador ver cómo otras personas han superado desafíos similares a los míos. ¡Gracias por esta invaluable fuente de motivación!",
-        imageUrl: "/profile2.png",
-    },
-    {
-        id: 3,
-        name: "María García",
-        description:
-            "Excelente recurso para obtener opiniones auténticas sobre diferentes productos y servicios. Me ha ayudado mucho en mis compras en línea. ¡Bravo por este sitio!",
-        imageUrl: "/profile3.png",
-    },
-    {
-        id: 4,
-        name: "Laura Snow",
-        description:
-            "¡Qué descubrimiento tan fantástico! Los testimonios aquí son honestos y detallados. Me siento más seguro al tomar decisiones después de leer las experiencias compartidas por otros usuarios.",
-        imageUrl: "/profile4.png",
-    },
-    {
-        id: 5,
-        name: "Carlos Sánchez",
-        description:
-            "Una joya en la web. Los testimonios son fáciles de encontrar y están bien organizados. ¡Definitivamente mi destino número uno cuando necesito referencias confiables!",
-        imageUrl: "/profile5.png",
-    },
-    {
-        id: 6,
-        name: "Antonio Martínez",
-        description:
-            "¡Fantástico recurso para aquellos que buscan validación antes de tomar decisiones importantes! Los testimonios aquí son veraces y realmente útiles. ¡Gracias por simplificar mi proceso de toma de decisiones!",
-        imageUrl: "/profile6.png",
-    },
 ];
